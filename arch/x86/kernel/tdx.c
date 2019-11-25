d9c2d0309a59221407906e6327f225a416c12f18
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2019-11-25 21:01:42 +0300

    x86/tdx: Handle CPUID via #VE
    When running virtualized, the CPUID instruction is handled differently
    based on the leaf being accessed.  The behavior depends only on the
    leaf and applies equally to both kernel/ring-0 and userspace/ring-3
    execution of CPUID. Historically, there are two basic classes:
    
     * Leaves handled transparently to the guest
     * Leaves handled by the VMM
    
    In a typical guest without TDX, "handled by the VMM" leaves cause a
    VMEXIT.  TDX replaces these VMEXITs with a #VE exception in the guest.
    The guest typically handles the #VE by making a hypercall to the VMM.
    
    The TDX module specification [1], section titled "CPUID Virtualization"
    talks about a few more classes of CPUID handling. But, for the purposes
    of this patch, the "handled transparently" CPUID leaves are all lumped
    together because the guest handling is the same.
    
    [1] - https://software.intel.com/content/dam/develop/external/us/en/documents/tdx-module-1.0-public-spec-v0.931.pdf
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -150,6 +150,31 @@ static int tdx_write_msr_safe(unsigned int msr, unsigned int low,
 	return ret ? -EIO : 0;
 }
 
+static u64 tdx_handle_cpuid(struct pt_regs *regs)
+{
+	struct tdx_hypercall_output out = {0};
+	u64 ret;
+
+	/*
+	 * Emulate CPUID instruction via hypercall. More info about
+	 * ABI can be found in TDX Guest-Host-Communication Interface
+	 * (GHCI), section titled "VP.VMCALL<Instruction.CPUID>".
+	 */
+	ret = _tdx_hypercall(EXIT_REASON_CPUID, regs->ax, regs->cx, 0, 0, &out);
+
+	/*
+	 * As per TDX GHCI CPUID ABI, r12-r15 registers contains contents of
+	 * EAX, EBX, ECX, EDX registers after CPUID instruction execution.
+	 * So copy the register contents back to pt_regs.
+	 */
+	regs->ax = out.r12;
+	regs->bx = out.r13;
+	regs->cx = out.r14;
+	regs->dx = out.r15;
+
+	return ret;
+}
+
 unsigned long tdx_get_ve_info(struct ve_info *ve)
 {
 	struct tdx_module_output out = {0};
@@ -192,6 +217,9 @@ int tdx_handle_virtualization_exception(struct pt_regs *regs,
 		break;
 	case EXIT_REASON_MSR_WRITE:
 		ret = tdx_write_msr_safe(regs->cx, regs->ax, regs->dx);
+		break;
+	case EXIT_REASON_CPUID:
+		ret = tdx_handle_cpuid(regs);
 		break;
 	default:
 		pr_warn("Unexpected #VE: %lld\n", ve->exit_reason);
