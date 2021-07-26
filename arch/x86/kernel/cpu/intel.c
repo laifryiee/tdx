f3f25b44b4ba7ac57d1ef2e19e87f3b0acb20fd7
Andi Kleen <ak@linux.intel.com>
2021-07-26 11:52:16 -0700

    x86/tdx: Make RDRAND/RDSEED loop forever
    The hardware random generator behind RDRAND/RDSEED has limited
    bandwidth and can be in theory starved for some time by other cores.
    That's an potential attack against a TDX guest's random number
    generation. The RDRAND/RDSEED users generally fall back to some other
    method if the default retry loop (10 retries) fails, but these other
    methods are usually guessable by a malicious host (like RDTSC or
    interrupt timing or some other methods), which might help the host with
    guessing
    
    Just make them loop forever inside TDX until a valid random number
    is retrieved.
    
    While that's a potential DOS, a malicious host has already other
    ways to DOS. It cannot be used for a DOS from other guests, because
    those will be eventually interrupted, which allows this guest to make
    progress.
    
    Note this only affects the kernel users.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/cpu/intel.c
+++ arch/x86/kernel/cpu/intel.c
@@ -89,6 +89,7 @@ bool intel_cc_platform_has(enum cc_attr attr)
 	case CC_ATTR_MEM_ENCRYPT:
 	case CC_ATTR_GUEST_SECURE_TIME:
 	case CC_ATTR_GUEST_CPUID_FILTER:
+	case CC_ATTR_GUEST_RAND_LOOP:
 		return tdx_guest_enabled;
 	case CC_ATTR_GUEST_DEVICE_FILTER:
 		return tdx_filter_enabled() && tdx_guest_enabled;
